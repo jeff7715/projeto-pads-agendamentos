bdaf328ff6472d0c3bb87a0ef391a1bc7839f335
Everson Silva <eversonsilva9799@gmail.com>
2019-12-02 09:45:10 -0300

    interface gráfica agendamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AgendamentoComponent } from './components/agendamento/agendamento.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'agendamento', component: AgendamentoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
